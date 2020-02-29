018942bea798a1df1e12d44666d763f2ed0ae2c2
Pablo Sempere <pablo.sempere.frutos@gmail.com>
2020-02-29 23:01:08 +0100

    Creada vista de lista

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import VistaIndice
+from .views import VistaIndice, VistaLista
 
 urlpatterns = [
     path('', VistaIndice.as_view()),
+    path('entradas/', VistaLista.as_view()),
 ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,5 +1,12 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
+
+from .models import Entrada
 
 
 class VistaIndice(TemplateView):
     template_name = 'index.html'
+
+
+class VistaLista(ListView):
+    model = Entrada
+    template_name = 'entradas.html'
