14c27cc73977ffd0fb4893250228d5d47f779cb3
Pablo Sempere <pablo.sempere.frutos@gmail.com>
2020-02-29 17:06:52 +0100

    Agregado modelo Entrada al admin

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Entrada
+
+
+@admin.register(Entrada)
+class EntradaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'texto')
