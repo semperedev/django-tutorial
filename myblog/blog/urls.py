97f9d79fac4c687b57633f26bd1f5b8560eca94a
Pablo Sempere <pablo.sempere.frutos@gmail.com>
2020-02-29 23:24:13 +0100

    Agregados enlaces

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import VistaIndice, VistaLista, VistaDetalle
 
 urlpatterns = [
-    path('', VistaIndice.as_view()),
-    path('entradas/', VistaLista.as_view()),
-    path('entrada/<pk>/', VistaDetalle.as_view()),
+    path('', VistaIndice.as_view(), name='indice'),
+    path('entradas/', VistaLista.as_view(), name='lista'),
+    path('entrada/<pk>/', VistaDetalle.as_view(), name='detalle'),
 ]
